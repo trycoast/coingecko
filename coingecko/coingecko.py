1c4185e7c3b4d21a00977baca7b04f7b93b908e1
Erik <meandiligence@gmail.com>
2022-06-21 01:23:29 +0300

    added headers to fetch method

--- coingecko/coingecko.py
+++ coingecko/coingecko.py
@@ -23,7 +23,8 @@ def fetch(endpoint: str) -> dict:
     Returns:
         dict: data from endpoint
     """
-    return requests.get(f'{URL}/{endpoint}').json()
+    headers = {'accept': 'application/json', 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'}
+    return requests.get(f'{URL}/{endpoint}', headers=headers).json()
 
 
 def get_historical_price(coinid: str, date: str) -> float:
